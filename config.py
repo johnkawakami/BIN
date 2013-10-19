a7c040509679b0bc0362649046990f5b466ec0b2
johnk <johnk@riceball.com>
2013-10-19 03:59:55 -0700

    unified exporting

---
+++ config.py
@@ -0,0 +1,19 @@
+listAdminEmail = "johnk@riceball.com"
+listAdminPassword = "nyc2011"
+domainsPck = "/tmp/domains.pck"
+virtualDb = "/var/postfix/virtual.db"
+mailBase = "/mnt/backup/mail"
+subscriberBase = "/mnt/backup/ezmlm-subscribers/"
+mailmanBase = "/var/lib/mailman/"
+mailmanBin = mailmanBase + "bin/"
+mailmanArchive = mailmanBase + "archives/private"
+
+EZMLM2MBOX = "./ezmlm2mbox"
+EZMLMLIST = "./ezmlmlist"
+NEWLIST = mailmanBin + "newlist"
+ADD_MEMBERS = mailmanBin + "add_members"
+ARCH = mailmanBin + "arch"
+LIST_LISTS = mailmanBin + "list_lists"
+RMLIST = mailmanBin + "rmlist"
+
+
